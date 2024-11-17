022c66e4693af982e3c0909b3d50798b697ca305
olgak <olgagpoulou@gmail.com>
2024-11-17 13:05:05 +0200

    1st edition

---
+++ users/urls.py
@@ -0,0 +1,9 @@
+
+from django.contrib.messages import api
+from django.urls import path
+from .views import RegisterView
+
+urlpatterns = [
+
+    path('register/', RegisterView.as_view(), name='register'),
+]
