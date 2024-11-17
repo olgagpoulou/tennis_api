05dc7fcb892a3ffdf4b931d25ab2a8fa8b4e9fdc
olgak <olgagpoulou@gmail.com>
2024-11-17 15:12:05 +0200

    make api: http://localhost:8000/api/login/ http://localhost:8000/api/register/ http://localhost:8000/api/user http://localhost:8000/api/logout

--- tennis/settings.py
+++ tennis/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'users',
 
 ]
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -132,3 +134,10 @@ AUTH_USER_MODEL='users.User'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_ALL_ORIGINS = True
+
+
+
+
--- users/views.py
+++ users/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from .serializers import UserSerializer
 from rest_framework.response import Response
+from .models import User
+from rest_framework.exceptions import AuthenticationFailed
+import jwt, datetime
 
 # Create your views here.
 class RegisterView(APIView):
@@ -13,3 +16,69 @@ class RegisterView(APIView):
 
 
 # Create your views here.
+class LoginView(APIView):
+    def post(self, request):
+        email=request.data['email']
+        password=request.data['password']
+        user=User.objects.filter(email=email).first()
+
+        if user is None:
+            raise AuthenticationFailed('User not found')
+
+        if not user.check_password(password):
+            raise AuthenticationFailed('Incorrect password')
+
+        payload={
+            'id': user.id,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
+            'iat': datetime.datetime.utcnow()
+
+        }
+        token = jwt.encode(payload, 'secret', algorithm='HS256')
+
+        # Δημιουργία απάντησης με το Response από το Django REST Framework
+
+        response = Response({
+            'message': 'Login successful'
+        })
+
+        # Ορισμός του JWT token στο cookie
+        response.set_cookie(
+            key='jwt',  # Το όνομα του cookie
+            value=token,  # Το περιεχόμενο του JWT token
+            httponly=True,  # Για να είναι ασφαλές (δεν μπορεί να προσπελαστεί από JavaScript)
+            max_age=datetime.timedelta(minutes=60),  # Η διάρκεια του cookie
+
+        )
+
+        return response
+
+class UserView(APIView):
+    def get(self, request):
+        token = request.COOKIES.get('jwt')
+
+        if not token:
+            raise AuthenticationFailed('Token not found')
+        try:
+            payload = jwt.decode(token, 'secret', algorithms=['HS256'])
+
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed('Invalid token')
+
+
+        user = User.objects.filter(id=payload['id']).first()
+        serializer = UserSerializer(user)
+
+        return Response(serializer.data)
+
+
+
+
+class LogoutView(APIView):
+    def post(self, request):
+        response = Response()
+        response.delete_cookie('jwt')
+        response.data={
+            'message': 'Logout successful'
+        }
+        return response
